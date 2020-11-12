00f0e774f5ff2e2792df0605d3776651a5db9688
JonathanF98 <63465753+JonathanF98@users.noreply.github.com>
2020-11-11 21:36:04 -0600

    Add files via upload

--- source.py
+++ source.py
@@ -1,8 +1,23 @@
 #Possible shebang would go here
-import serial as ser
 
+#########
+#Imports
+#########
+import serial as ser			#imports pySerial for comm with LCD
+#import RPi.GPIO_NP as GPIO		#imports RPi.GPIO_NP to talk with GPIO pins
+currentForm = 0					#global variable that should follow the current LCD screen form
+										#Make sure to keep track of this value as it will change
+										#between fucntions
 
+############################# FUNCTION - DEFINITIONS ################################
 def boot_sequence():
+	"""
+		Anything that the NanoPi will need to initialize
+		before proper function of the device
+	"""
+	GPIO.setmode(GPIO.BOARD)
+	pin_declarations()
+	
 def network_detect():
 	"""
 		All network detection functionality using helper
@@ -14,27 +29,33 @@ def rf_detect():
 		Should be copy paste, but will see about
 		importing as class later
 	"""
-def use_mode_select():
+#############################
+# Helper Functions For Modes
+#############################
+def user_mode_select():
 	
-
-def led_on(index):
-def led_off(index):
-def change_form(form):
-
-
+#def led_on(index):
+	
+#def led_off(index):
+	
+def change_form(form): 			#Allows change of global variable "change_form" without keyword
+	global currentForm 
+	currentForm = form
+	return None
+	
+def pin_declarations():
+	GPIO.setup()
+	
+#################################### MAIN FUNCTION ##################################
 
 def main():
 	print("This is the main function")
 	boot_sequence()
 	
-	while(1)
-	
-		Select = {
-			0: network_detect(),
-			1: rf_detect()
-			}
+	while(1):
+		if(currentForm == 0):
+			user_mode_select()
+			
 		
-		use_mode_select()
-	
 if __name__ == "__main__":
 	main()
