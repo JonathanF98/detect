fd88094492c711ab1842d928ccf3c67117dfde51
JonathanF98 <63465753+JonathanF98@users.noreply.github.com>
2020-11-11 19:59:41 -0600

    Add files via upload

---
+++ source.py
@@ -0,0 +1,40 @@
+#Possible shebang would go here
+import serial as ser
+
+
+def boot_sequence():
+def network_detect():
+	"""
+		All network detection functionality using helper
+		functions, user input, and serial talk to the display
+	"""
+def rf_detect():
+	"""
+		This is where the GNU Radio code goes
+		Should be copy paste, but will see about
+		importing as class later
+	"""
+def use_mode_select():
+	
+
+def led_on(index):
+def led_off(index):
+def change_form(form):
+
+
+
+def main():
+	print("This is the main function")
+	boot_sequence()
+	
+	while(1)
+	
+		Select = {
+			0: network_detect(),
+			1: rf_detect()
+			}
+		
+		use_mode_select()
+	
+if __name__ == "__main__":
+	main()
