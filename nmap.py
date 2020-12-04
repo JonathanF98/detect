7e548ac816005efe935ee632e798fbd4862e67f2
JonathanF98 <63465753+JonathanF98@users.noreply.github.com>
2020-12-03 20:08:37 -0600

    Add files via upload

--- nmap.py
+++ nmap.py
@@ -1,4 +1,272 @@
-mport subprocess as sp
+# Possible shebang would go here
+
+############################# IMPORTS & GLOBALS ################################
+import subprocess as sp
+import serial			          # imports pySerial for comm with LCD
+import RPi.GPIO as GPIO		# imports RPi.GPIO_NP to talk with GPIO pins
+import time
+ser = serial.Serial('/dev/ttyS1', 115200)
+currentForm = 0					    # global variable that should follow the current LCD screen form
+													# Make sure to keep track of this value as it will change
+													# between functions
+# FORM 0 INDEX
+FORM_0_INDEX			= bytearray(b'\x00')
+# FORM 1 INDEXES
+FORM_1_INDEX			= bytearray(b'\x01')			# replace "index with hex code such as 00,01,1B, ETC."
+NETWORK_DETECT_LED		= bytearray(b'\x00')
+RF_DETECT_LED 			= bytearray(b'\x01')
+FORM1_EXIT_LED 		  	= bytearray(b'\x02')
+# FORM 2 INDEXES
+FORM_2_INDEX			= bytearray(b'\x02')
+NETWORK_STRING_0		= bytearray(b'\x00')
+NETWORK_STRING_1	  	= bytearray(b'\x01')
+NETWORK_STRING_2		= bytearray(b'\x02')
+NETWORK_STRING_3		= bytearray(b'\x03')
+NETWORK_STRING_4		= bytearray(b'\x04')
+NETWORK_LED_0			= bytearray(b'\x03')
+NETWORK_LED_1			= bytearray(b'\x04')
+NETWORK_LED_2			= bytearray(b'\x05')
+NETWORK_LED_3			= bytearray(b'\x06')
+NETWORK_LED_4			= bytearray(b'\x07')
+# FORM 3 INDEXES
+FORM_3_INDEX			= bytearray(b'\x03')
+ENTER_KEY_LED			= bytearray(b'\x14')
+KEYPAD_LED_0			= bytearray(b'\x08')
+KEYPAD_LED_1			= bytearray(b'\x09')
+KEYPAD_LED_2			= bytearray(b'\x0A')
+KEYPAD_LED_3			= bytearray(b'\x0B')
+KEYPAD_LED_4			= bytearray(b'\x0C')
+KEYPAD_LED_5			= bytearray(b'\x0D')
+KEYPAD_LED_6			= bytearray(b'\x0E')
+KEYPAD_LED_7			= bytearray(b'\x0F')
+KEYPAD_LED_8			= bytearray(b'\x10')
+KEYPAD_LED_9			= bytearray(b'\x11')
+KEYPAD_LED_10			= bytearray(b'\x12')
+KEYPAD_LED_11			= bytearray(b'\x13')
+PASSWORD_STRING			= bytearray(b'\x05')
+# FORM 4 INDEXES
+FORM_4_INDEX			= bytearray(b'\x04')
+MAC_ADDRESS_STRING_0	= bytearray(b'\x06')
+DEVICE_STRING_0			= bytearray(b'\x07')
+MAC_ADDRESS_STRING_1	= bytearray(b'\x08')
+DEVICE_STRING_1			= bytearray(b'\x09')
+MAC_ADDRESS_STRING_2	= bytearray(b'\x0A')
+DEVICE_STRING_2			= bytearray(b'\x0B')
+NEXT_FORM_LED_0			= bytearray(b'\x15')
+
+# FORM 5 INDEXES
+FORM_5_INDEX			= bytearray(b'\x05')
+MAC_ADDRESS_STRING_3	= bytearray(b'\x0C')
+DEVICE_STRING_3			= bytearray(b'\x0D')
+MAC_ADDRESS_STRING_4	= bytearray(b'\x0E')
+DEVICE_STRING_4			= bytearray(b'\x0F')
+MAC_ADDRESS_STRING_5	= bytearray(b'\x10')
+DEVICE_STRING_5			= bytearray(b'\x11')
+NEXT_FORM_LED_1 		= bytearray(b'\x16')
+
+# FORM 6 INDEXES
+FORM_6_INDEX			= bytearray(b'\x06')
+MAC_ADDRESS_STRING_6	= bytearray(b'\x12')
+DEVICE_STRING_6			= bytearray(b'\x13')
+MAC_ADDRESS_STRING_7	= bytearray(b'\x14')
+DEVICE_STRING_7			= bytearray(b'\x15')
+MAC_ADDRESS_STRING_8	= bytearray(b'\x16')
+DEVICE_STRING_8			= bytearray(b'\x17')
+NEXT_FORM_LED_2			= bytearray(b'\x17')
+
+# FORM 7 INDEXES
+FORM_7_INDEX			= bytearray(b'\x07')
+PROXIMITY_LED_0			= bytearray(b'\x18')
+PROXIMITY_LED_1			= bytearray(b'\x19')
+PROXIMITY_LED_2			= bytearray(b'\x1A')
+PROXIMITY_LED_3			= bytearray(b'\x1B')
+LED_DIGITS_0			= bytearray(b'\x00')
+ELIPSES_STRING			= bytearray(b'\x18')
+
+############################# FUNCTION - DEFINITIONS ################################
+
+#############################
+# Helper Functions For Modes
+#############################
+
+
+def ok_button_pressed():
+	if GPIO.input(4):
+		time.sleep(0.3)
+		return 1
+	else:
+		return 0
+
+
+def up_button_pressed():
+	if GPIO.input(10):
+		time.sleep(0.3)
+		return 1
+	else:
+		return 0
+
+
+def down_button_pressed():
+	if GPIO.input(18):
+		time.sleep(0.3)
+		return 1
+	else:
+		return 0
+
+
+# def left_button_pressed():
+# 	if GPIO.input(18):
+# 		time.sleep(0.3)
+# 		return 1
+# 	else:
+# 		return 0
+
+
+# def right_button_pressed():
+# 	if GPIO.input(4):
+# 		time.sleep(0.3)
+# 		return 1
+# 	else:
+# 		return 0
+
+
+def generate_CHKSUM(command):
+	for x in range(len(command)):
+		if (x == 0):
+			result = command[x]^command[x+1]
+		elif(x >= 2):
+			result = result^command[x]
+	return result
+
+
+def led_on(LED):
+	command = bytearray(b'\x01\x13')
+	command.extend(LED)
+	command.extend(b'\x00\x01')
+	CHKSUM = generate_CHKSUM(command)
+	command.append(CHKSUM)
+	ser.write(command)
+	response = ser.read()
+	print(response)
+
+
+def led_off(LED):
+	command = bytearray(b'\x01\x13')
+	command.extend(LED)
+	command.extend(b'\x00\x00')
+	CHKSUM = generate_CHKSUM(command)
+	command.append(CHKSUM)
+	ser.write(command)
+	response = ser.read()
+	print(response)
+
+
+def default_keypad_state():
+	led_off(KEYPAD_LED_0)
+	led_off(KEYPAD_LED_1)
+	led_off(KEYPAD_LED_2)
+	led_off(KEYPAD_LED_3)
+	led_off(KEYPAD_LED_4)
+	led_off(KEYPAD_LED_5)
+	led_off(KEYPAD_LED_6)
+	led_off(KEYPAD_LED_7)
+	led_off(KEYPAD_LED_8)
+	led_off(KEYPAD_LED_9)
+	led_off(KEYPAD_LED_10)
+	led_off(KEYPAD_LED_11)
+
+
+def default_mode_state():
+	led_off(NETWORK_DETECT_LED)
+	led_off(RF_DETECT_LED)
+	led_off(FORM1_EXIT_LED)
+
+
+def default_network_state():
+	led_off(NETWORK_LED_0)
+	led_off(NETWORK_LED_1)
+	led_off(NETWORK_LED_2)
+	led_off(NETWORK_LED_3)
+	led_off(NETWORK_LED_4)
+
+
+# changing digits
+# def change_dig(value):
+# 	command = bytearray(b'\x01\x0F\x00')
+# 	#insert value conversion
+# 	CHKSUM = generate_CHKSUM(command)
+# 	command.append(CHKSUM)
+#  	ser.write(command)
+
+
+# changing strings
+def change_string(index, message):
+	command = bytearray(b'\x02')
+	command.extend(index)
+	command.append(len(message))
+	command.extend(message.encode("ascii"))
+	command.append(generate_CHKSUM(command))
+	ser.write(command)
+	response = ser.read()
+	print(response)
+
+
+def change_form(form): 			# Allows change of global variable "currentForm" without keyword
+	global currentForm
+	currentForm = form
+	command = bytearray(b'\x01\x0A')
+	if(form == 0):
+		command.extend(FORM_0_INDEX)
+	if(form == 1):
+		command.extend(FORM_1_INDEX)								# add in binary, more than one 1, equals 0. otherwise 1
+	elif(form == 2):
+		command.extend(FORM_2_INDEX)
+	elif(form == 3):
+		command.extend(FORM_3_INDEX)
+	elif(form == 4):
+		command.extend(FORM_4_INDEX)
+	elif(form == 5):
+		command.extend(FORM_5_INDEX)
+	elif(form == 6):
+		command.extend(FORM_6_INDEX)
+	elif(form == 7):
+		command.extend(FORM_7_INDEX)
+	command.extend(bytearray(b'\x00\x00'))
+	CHKSUM = generate_CHKSUM(command)
+	command.append(CHKSUM)
+	ser.write(command)
+	response = ser.read()
+	print(response)
+	print("form ==> ", form)
+	return None
+
+
+change_form(4)
+
+print("entering network scan")
+print("scanning network . . .")
+
+MAC_DICT = {0: MAC_ADDRESS_STRING_0,
+	1: MAC_ADDRESS_STRING_1,
+	2: MAC_ADDRESS_STRING_2,
+	3: MAC_ADDRESS_STRING_3,
+	4: MAC_ADDRESS_STRING_4,
+	5: MAC_ADDRESS_STRING_5,
+	6: MAC_ADDRESS_STRING_6,
+	7: MAC_ADDRESS_STRING_7,
+	8: MAC_ADDRESS_STRING_8
+	}
+DEVICE_DICT = { 0: DEVICE_STRING_0,
+	    1: DEVICE_STRING_0,
+	    2: DEVICE_STRING_0,
+	    3: DEVICE_STRING_0,
+	    4: DEVICE_STRING_0,
+	    5: DEVICE_STRING_0,
+	    6: DEVICE_STRING_0,
+	    7: DEVICE_STRING_0,
+	    8: DEVICE_STRING_0
+	    }
+
 nmap_f = open("nmap.txt", "w")
 nmap_f2 = open("nmap2.txt", "w")
 sp.call(['ip', 'a'], stdout=nmap_f)
@@ -26,8 +294,28 @@ print(MAC_list)
 Devices = []
 MAC_addresses = []
 for x in MAC_list:
-        MAC_addresses.append(x[:30])
-        Devices.append(x[30:])
+	MAC_addresses.append(x[:30])
+	Devices.append(x[30:])
+for y in range(len(MAC_addresses)):
+	change_string(MAC_DICT[y], MAC_addresses[y])
+	change_string(DEVICE_DICT[y], Devices[y])
 
-print(MAC_addresses)
-print(Devices)
+led_on(NEXT_FORM_LED_0)
+led_on(NEXT_FORM_LED_1)
+led_on(NEXT_FORM_LED_2)
+
+change_form(4)
+while (1):
+	if (ok_button_pressed()):
+		break
+change_form(5)
+while (1):
+	if (ok_button_pressed()):
+		break
+change_form(6)
+while (1):
+	if (ok_button_pressed()):
+		break
+
+print("exiting network scan, status: SUCCESS")
+return 0
